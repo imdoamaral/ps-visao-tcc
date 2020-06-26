f7ea35ad3fdaf39bedcaff028e47337eff6e5b4a
IgorVVieira <igor.gutoo63@gmail.com>
2020-06-26 13:01:58 -0300

    Rotas para acesso corrigidas, mas header deu problema

--- src/routes.js
+++ src/routes.js
@@ -2,8 +2,25 @@ const express = require('express');
 const routes = express.Router();
 const ProductController = require('./controllers/ProductController');
 
+// Rotas para acessar páginas:
 routes.get('/', (req, res) => {
     return res.render('index');
+});
+
+routes.get('/cardapio', (req, res) => {
+    return res.render('cardapio');
+});
+
+routes.get('/carrinho', (req, res) => {
+    return res.render('carrinho');
+});
+
+routes.get('/contato', (req, res) => {
+    return res.render('contato');
+});
+
+routes.get('/quem_somos', (req, res) => {
+    return res.render('quem-somos');
 });
 
 routes.get('/products', ProductController.index);
