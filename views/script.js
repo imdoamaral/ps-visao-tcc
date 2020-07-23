9867cc15f8925a7e0132a881bea8344c14bc7de3
Israel Amaral <imdoamaral@gmail.com>
2020-07-23 12:28:40 -0300

    add evento onclick no btn_finalizar_compra

--- views/script.js
+++ views/script.js
@@ -287,6 +287,9 @@ function carregaListaPizzas() {
     btn_finalizarCompra.className = 'btn btn-primary'
     btn_finalizarCompra.href = '#'
     btn_finalizarCompra.innerHTML = 'Finalizar a compra '+'<i class="fas fa-check"></i>'
+    btn_finalizarCompra.onclick = function() {
+        alert('Esta funcinalidade estará disponível em breve!')
+    }
     
     linhaBotoes.insertCell(2).append(btn_finalizarCompra)
 
